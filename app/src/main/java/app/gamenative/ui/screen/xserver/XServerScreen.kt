0bf4fce2d254d26f2ef9abdd57f9e6dc6c2e11c7
Utkarsh Dalal <utkarsh.dalal@toptal.com>
2026-01-22 19:28:10 +0530

    attempt close app after quit game

--- app/src/main/java/app/gamenative/ui/screen/xserver/XServerScreen.kt
+++ app/src/main/java/app/gamenative/ui/screen/xserver/XServerScreen.kt
@@ -1864,9 +1864,8 @@ fun XServerScreen(
                 touchMouse = TouchMouse(getxServer())
                 keyboard = Keyboard(getxServer())
                 if (!bootToContainer) {
-                    renderer.setUnviewableWMClasses("explorer.exe")
                     // TODO: make 'force fullscreen' be an option of the app being launched
-                    if (container.executablePath.isNotBlank()) {
+                    if (!testGraphics && container.executablePath.isNotBlank()) {
                         renderer.forceFullscreenWMClass = Paths.get(container.executablePath).name
                     }
                     // Here, Ludashi calls setDriverInfo to use Adrenotools for the compositor
@@ -3287,7 +3286,7 @@ private fun setupXEnvironment(
                 guestProgramLauncherComponent.setSteamAppId(numericAppId.toString())
             }
         }
-        gameExecutable = "wine explorer /desktop=shell," + xServer.screenInfo + " " +
+        gameExecutable = "wine " +
             getWineStartCommand(context, appId, container, bootToContainer, testGraphics, appLaunchInfo, envVars, guestProgramLauncherComponent, gameSource, offline) +
             (if (container.execArgs.isNotEmpty()) " " + container.execArgs else "")
         preInstallCommands = PreInstallSteps.getPreInstallCommands(
